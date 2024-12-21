03cbba67718854e63973fd10f3eec05242209878
KotonBads <66117568+KotonBads@users.noreply.github.com>
2024-12-21 17:45:39 +0800

    [feat] add global styling

---
+++ internal/styles/styles.go
@@ -0,0 +1,23 @@
+package styles
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	BaseStyle = lipgloss.NewStyle().
+			Width(80).
+			Foreground(lipgloss.Color("7")).
+			TabWidth(2)
+	HeaderStyle = BaseStyle.
+			Bold(true).
+			Foreground(lipgloss.Color("12"))
+	TextStyle = BaseStyle.
+			Foreground(lipgloss.Color("10"))
+	SynonymStyle = BaseStyle.
+			Foreground(lipgloss.Color("10"))
+	AntonymStyle = BaseStyle.
+			Foreground(lipgloss.Color("9"))
+	ExampleStype = BaseStyle.
+			PaddingLeft(2)
+	WordStyle = HeaderStyle.
+			Foreground(lipgloss.Color("15"))
+)
