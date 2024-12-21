20aa73a7ed18dfa4f7115288eca7235e926e2e42
KotonBads <66117568+KotonBads@users.noreply.github.com>
2024-12-21 20:17:11 +0800

    [feat] add timeout

--- api/api.go
+++ api/api.go
@@ -1,5 +1,25 @@
 package api
 
+import (
+	"fmt"
+	"time"
+)
+
 func GetDefinition(provider DictionaryProvider, query string) (Word, error) {
-	return provider.Fetch(query)
+	result := make(chan Word, 1)
+	error := make(chan error, 1)
+	go func() {
+		res, err := provider.Fetch(query)
+		if err != nil {
+			error <- err
+		}
+		result <- res
+		error <- nil
+	}()
+	select {
+	case res := <-result:
+		return res, <-error
+	case <-time.After(30 * time.Second):
+		return <-result, fmt.Errorf("request timeout")
+	}
 }
